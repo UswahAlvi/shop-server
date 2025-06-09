35309d85e12031c189d01a9e96e588a6aecdbfc3
uswahalvi <uswaaalvi@gmail.com>
2025-06-09 13:55:02 -0700

    initial commit

--- middlewares/session.js
+++ middlewares/session.js
@@ -1,5 +1,7 @@
 import { v4 as uuidv4 } from 'uuid';
 
+const isProd = process.env.NODE_ENV === 'production';
+
 export const ensureSession = (req, res, next) => {
   let sessionId = req.cookies.sessionId;
 
@@ -8,7 +10,8 @@ export const ensureSession = (req, res, next) => {
     res.cookie('sessionId', sessionId, {
       httpOnly: true,
       maxAge: 1000 * 60 * 60 * 24 * 7, // 7 days
-      sameSite: 'Lax',
+      sameSite: isProd ? 'None' : 'Lax',
+      secure: isProd,
     });
   }
 
